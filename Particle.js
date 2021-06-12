d0b0a3eeba133b5074302895c4014198038786c7
Nityaa14 <73293921+Nityaa14@users.noreply.github.com>
2021-06-12 13:58:40 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,28 @@
+class Particle {
+    constructor(x, y, r)
+     var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+         this.radi = r;
+        this.body = Bodies.circle(x, y, this.radi, options);
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world, this.body);
+
+    display() {
+    
+        var pos = this.body.position;
+        var angle = this.body.angle;
+    
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        //fill("pink");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radi,this.radi);
+        pop();
+    }
+};
